100d4d8198a6fce48ab085c50e97d2256713d6ea
Aegis Legion <106640451+aegislegion@users.noreply.github.com>
2023-07-18 11:02:37 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,29 @@
 var Ingresos;
 var Egresos;
 var utilidad;
-var resultado;
+var impuesto;
 
 function Calcular() {
 var Ingresos = parseFloat(document.getElementById("num1").value);
 var Egresos = parseFloat(document.getElementById("num2").value);
 var utilidad = Ingresos - Egresos;
+var impuesto=0;
 
-if (isNaN(Ingresos)) {
-        alert("Por favor, ingresa un valor numérico válido.");
-        return;
-    }
-
-    
 if  ((0 <  utilidad ) && (utilidad< 9950)) {
-    resultado= utilidad*0.10; 
+    impuesto= utilidad*0.10; 
 } else if ((9950<utilidad)&&(utilidad < 40525)){
-    resultado=((utilidad-9950)*0.12) + 995;
+    impuesto=((utilidad-9950)*0.12) + 995;
 } else if ((40525<utilidad)&&(utilidad < 86375)){
-    resultado=((utilidad-40525)*0.22) + 4663.88;
+    impuesto=((utilidad-40525)*0.22) + 4663.88;
 } else if ((86375<utilidad)&&(utilidad < 164725)){
-    resultado= ((utilidad-86375)*0.24) + 14750.66;
+    impuesto= ((utilidad-86375)*0.24) + 14750.66;
 } else if ((164725<utilidad)&&(utilidad < 209425)){
-    resultado=((utilidad-164725)*0.32) + 33602.42;
+    impuesto=((utilidad-164725)*0.32) + 33602.42;
 } else if ((209425<utilidad)&&(utilidad < 523600)){
-    resultado=((utilidad-209425)*0.35) + 47842.10;
+    impuesto=((utilidad-209425)*0.35) + 47842.10;
 } else{
-    resultado=((utilidad-523601)*0.37) + 157803;};
+    impuesto=((utilidad-523601)*0.37) + 157803;};
 
-resultado.innerHTML="Los impuestos a pagar son:$ {resultado}"
+alert("El resultado es:"+impuesto);
 }
+
